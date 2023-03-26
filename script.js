0c6d9e1b1a6b4a3f69c6c1072261b1a999834d91
Ricki Weigel <ricki.weigel@gmx.de>
2023-03-26 17:07:46 +0200

    #

--- script.js
+++ script.js
@@ -2,10 +2,4 @@ function highlightSelectedNav(id){
     setTimeout(() => {
         document.getElementById(`${id}`).classList.add("selected");  
     }, 10);
-    console.log(typeof id);
-    // document.getElementById(`summary`).classList.remove("selected");
-    // document.getElementById(`board`).classList.remove("selected");
-    // document.getElementById(`addTask`).classList.remove("selected");
-    // document.getElementById(`contacts`).classList.remove("selected");
-    // document.getElementById(`${id}`).classList.add("selected");
 }
