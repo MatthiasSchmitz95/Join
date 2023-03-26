6b59e0b440107b3e475a8eedcf83154adfdafa1e
Ricki Weigel <ricki.weigel@gmx.de>
2023-03-27 00:14:38 +0200

    design login

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let loginChecked;
+
 async function init(id){
     await includeHTML();
     highlightSelectedNav(id)
@@ -8,4 +10,14 @@ function highlightSelectedNav(id){
     setTimeout(() => {
         document.getElementById(`${id}`).classList.add("selected");  
     }, 10);
+}
+
+function loginCheckbox(){
+    if (loginChecked) {
+        document.getElementById('loginCheckbox').src = './assets/img/unchecked.png';
+        loginChecked=false
+    }else {
+        document.getElementById('loginCheckbox').src = './assets/img/checked.png';
+        loginChecked=true
+    }
 }
