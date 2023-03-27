69b419dc7ea13e4db9c3845670204ec208284f01
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-03-27 12:27:45 +0200

    adding contact card

---
+++ contacts.js
@@ -0,0 +1,6 @@
+function showCard(){
+    document.getElementById('contact-card').style.display ='';
+    document.getElementById('desktop').style.opacity = '0.5';
+    document.getElementById('contact-card').style.opacity ='1';
+    
+}
