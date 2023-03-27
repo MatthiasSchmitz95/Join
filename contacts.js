73c08fb257ae9f832aa0a30f0b74e1c26f1622e0
Matthias Schmitz <m6schmitz123@gmail.com>
2023-03-27 23:08:04 +0200

    style changes worked on contact-card

--- contacts.js
+++ contacts.js
@@ -1,6 +1,4 @@
 function showCard(){
     document.getElementById('contact-card').style.display ='';
-    document.getElementById('desktop').style.opacity = '0.5';
-    document.getElementById('contact-card').style.opacity ='1';
     
 }
