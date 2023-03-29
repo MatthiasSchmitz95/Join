1d14a3042474d6bd0a3316a6edfc07c7763fa1fd
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-03-29 11:37:29 +0200

    contacts.js function for creating new contacts

--- contacts.js
+++ contacts.js
@@ -1,9 +1,40 @@
-function showCard(){
-    document.getElementById('contact-card').classList ='add-contact-card';
-    
+let contactName;
+let email;
+let phone;
+let contact_nr = 0;
+let contacts = [
+
+];
+
+function showCard() {
+    document.getElementById('contact-card').classList = 'add-contact-card';
+
 }
 
-function closeContactCard(){
-    document.getElementById('contact-card').classList ='hidden';
+function closeContactCard() {
+    document.getElementById('contact-card').classList = 'hidden';
+
+}
+
+function getInputValues(){
+    let contact_email = document.getElementById('contact-email');
+    let contact_phone = document.getElementById('contact-phone');
+    let contact_name = document.getElementById('contact-name');
+    contactName = contact_name.value;
+    email = contact_email.value;
+    phone = contact_phone.value;
+}
+
+function updateContactNr(){
+    contact_nr++;
+    console.log(contact_nr);
+}
+
+function CreateNewContact() {
+    getInputValues();
+    let contact_obj = { 'name': contactName, 'email': email, 'phone': phone };
+    contacts.push(contact_obj);
+    console.log(contacts);
+    updateContactNr();
 
 }
