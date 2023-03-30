c1db697b7ca7554c66d41e045ee40eb9066a0a55
Ricki Weigel <ricki.weigel@gmx.de>
2023-03-31 01:37:05 +0200

    #

--- js/login.js
+++ js/login.js
@@ -138,7 +138,6 @@ function randomUserColor() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
-
   let rgbColor = `rgb(${r},${g},${b},)`;
   return rgbColor;
 }
