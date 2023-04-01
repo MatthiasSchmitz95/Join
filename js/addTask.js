cd2090d4a84be950fcab749db68e5fe7a8cf17e3
YeHtutAung <yehaung.nov@gmail.com>
2023-04-01 22:57:12 +0200

    dropdown updated

--- js/addTask.js
+++ js/addTask.js
@@ -1,83 +1,105 @@
-let team = ["Name1", "Name2", "Name3"];
+/*let team = ["Name1", "Name2", "Name3"];*/
 
 // modify calendar so you can only select current date or date in the future
+/*let date = document.getElementById("date");
+
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, "0");
 let mm = String(today.getMonth() + 1).padStart(2, "0");
 let yyyy = today.getFullYear();
 today = yyyy + "-" + mm + "-" + dd;
-document.getElementById("date").min = today;
+document.getElementById("date").min = today;*/
 
-let date = document.getElementById("date");
+/**
+ * Category
+ */
+var categoriesArray = ['New Category', 'Sales', 'Marketing'];
+var colorsArray = ['', 'red', 'blue'];
+var categoryInputContainer = document.getElementById('inputContainer');
 
-//category elements
-let categories = ["New Category", "Sales", "Marketing"];
-let dropdownContainer = document.getElementById("dropdownContainer");
-let categoryInput = document.getElementById("selectedCategory");
-let categoryContainerBorder = document.getElementById("selectedCategoryContainer");
+function renderCategory() {
+    let categoryList = document.getElementById('categoryList');
+    categoryList.innerHTML = "";
+    for (let i = 0; i < categoriesArray.length; i++) {
+        const category = categoriesArray[i];
+        const color = colorsArray[i];
 
-let colorDots = document.getElementsByClassName("circle"); //Elemente by className
-let assignedColors = [colorDots[0], colorDots[1], colorDots[2]];
-//let colorContainer = document.getElementById("colorContainer");
-
-categoryInput.addEventListener("click", function () {
-  dropdownContainer.style.display = "block";
-  categoryContainerBorder.style.borderColor = "black";
-  categoryContainerBorder.style.borderBottom = "none";
-  categoryContainerBorder.style.borderRadius = "10px 10px 0 0";
-  dropdownContainer.innerHTML = "";
-  for (let i = 0; i < categories.length; i++) {
-    let color = assignedColors[i];
-    dropdownContainer.innerHTML += /*html*/`
-    <div class="dropdownChoiceCategory">
-      <div class="categoryText">${categories[i]}</div>
-      ${color.outerHTML}
-    </div>`;
-  }
-});
-
-
-
-//assignTo elemetns
-let assignedToInput = document.getElementById("assignedTo");
-let contactsDropdown = document.getElementById("ContactsDropdownContainer");
-let assignedToContainerBorder = document.getElementById("assignToContainer");
-
-assignedToInput.addEventListener("click", function () {
-  contactsDropdown.style.display = "block";
-  assignedToContainerBorder.style.borderColor = "black";
-  assignedToContainerBorder.style.borderBottom = "none";
-  assignedToContainerBorder.style.borderRadius = "10px 10px 0 0";
-
-  contactsDropdown.innerHTML = "";
-  for (let i = 0; i < team.length; i++) {
-    let contact = team[i];
-    contactsDropdown.innerHTML += /*html*/`
-    <div class="dropdownChoiceContact">
-        <div class="assignText">${contact}</div>
-        <input type="checkbox" name="checkbox" />
-    </div>
-    `;
-  }
-});
-
-
-function mouseOverClearBtn(){
-  document.getElementById("clearTaskBtn").style.color = "#32daff";
+        categoryList.innerHTML += `
+        <div class="categoryAndColor" onclick="chooseCategory(${i}, '${category}', '${color}')" >
+            <div>${category}</div>
+            <div class="color"  style="background-color:${color}"></div>
+        </div>
+        `;
+    }
 }
 
-function mouseOutClearBtn(){
-  document.getElementById("clearTaskBtn").style.color = "black";
+function dropDown() {
+    /* document.getElementById('categoryList').classList.toggle('dropDownDisplay');*/
+    var categoryDropDown = document.getElementById('categoryList');
+
+
+    if (categoryDropDown.style.display == "block") {
+        categoryDropDown.style.display = "none";
+        categoryInputContainer.style.border = "1px solid #D1D1D1";
+        categoryInputContainer.style.borderRadius = "10px";
+    } else {
+        categoryDropDown.style.display = "block";
+        categoryInputContainer.style.borderBottom = "none";
+        /* Four values */
+        /* top-left top-right bottom-right bottom-left */
+        categoryInputContainer.style.borderRadius = "10px 10px 0 0";
+    }
+
 }
 
-function getValueForTasks() {
-  selectElement = document.querySelector('#selectedCategory');
-  selectedCategory = selectElement.options[selectElement.selectedIndex].value;
+function chooseCategory(index, category, color) {
+    let input = document.getElementById('input');
+    input.value = '';
+    input.value = category;
+    document.getElementById('color').style.background = color;
+}
 
-  selectElement = document.querySelector('#selectedContact');
-  selectedContact = selectElement.options[selectElement.selectedIndex].value;
-  //document.querySelector('.output').textContent = output;
-  alert("Die ausgewählte Category ist(" + selectedCategory +
-    ") und der asugewählte Contact ist(" + selectedContact + ")"
-    + `Datum ist (${date.value})`);
+/**
+ * AssignTo 
+ */
+var assignToInputContainer = document.getElementById('contactInputContainer');
+var contactListArray = ['contact1', 'contact2', 'contact3'];
+
+function renderAssignTo(){
+    let assignedContactList = document.getElementById('assignedList');
+    assignedContactList.innerHTML = "";
+    for (let i = 0; i < contactListArray.length; i++) {
+        const contact = contactListArray[i];
+        
+        assignedContactList.innerHTML += `
+        <div class="assignedContact" onclick="chooseContact(${i}, '${contact}')" > 
+        <div>${contact}</div>
+        <input type="checkbox" name="checkbox" class="checkbox${i}"/>
+        </div>
+        `;
+    }
+}
+
+function dropDownAssignTo(){
+    /* document.getElementById('categoryList').classList.toggle('dropDownDisplay');*/
+    var assignToDropDown = document.getElementById('assignedList');
+
+    if (assignToDropDown.style.display == "block") {
+        assignToDropDown.style.display = "none";
+        assignToInputContainer.style.border = "1px solid #D1D1D1";
+        assignToInputContainer.style.borderRadius = "10px";
+    } else {
+        assignToDropDown.style.display = "block";
+        assignToInputContainer.style.borderBottom = "none";
+        /* Four values */
+        /* top-left top-right bottom-right bottom-left */
+        assignToInputContainer.style.borderRadius = "10px 10px 0 0";
+    }
+
+}
+
+function chooseContact(index, contact){
+    let inputAssignedContact = document.getElementById('assignInput');
+    inputAssignedContact.value = '';
+    inputAssignedContact.value = contact;
 }
