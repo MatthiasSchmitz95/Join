23cf885aea143001b79b61f54010d4517d61feb7
Matthias Schmitz <m6schmitz123@gmail.com>
2023-04-27 02:42:37 +0200

    fixed edit profile initilas

--- js/contacts.js
+++ js/contacts.js
@@ -70,7 +70,7 @@ function showContact(j, bothLetters) {
     document.getElementById('contact-circle').style.backgroundColor = `${color}`;
     document.getElementById('float-contact-name').innerHTML = `${contactName}`;
     document.getElementById('contact-information').innerHTML = `<h4>Contact information</h4>
-    <a onclick="editContactCard(${j});changeProfileImg(${j});"><img src="assets/img/edit-contact.png"><p>Edit contact</p></a>`;
+    <a onclick="editContactCard(${j});"><img src="assets/img/edit-contact.png"><p>Edit contact</p></a>`;
     document.getElementById('email').innerHTML = `${email}`;
     document.getElementById('phone').innerHTML = `${phone}`;
     document.getElementById('floating-contact-container').style.display = "";
@@ -80,13 +80,14 @@ function showContact(j, bothLetters) {
 function editContactCard(j) {  
     changeButtonTemplate(j);
     setInputfields(j);
+    changeProfileInitials(j);
     showCard();
 }
 
-function changeProfileImg(j){
-    let initials = [userAccounts][activeUser]['userContacts'][j]['letters'];
-    let color = [userAccounts][activeUser]['userContacts'][j]['color'];
-   // document.getElementById('icon-container').innerHTML = '';
+function changeProfileInitials(j){
+    let initials = userAccounts[activeUser]['userContacts'][j]['letters'];
+    let color = userAccounts[activeUser]['userContacts'][j]['color'];
+   // document.getElementById('icon-container').innerHTML = `<div class="icon-container">`;
     document.getElementById('icon-container').innerHTML = `${initials}`;
     document.getElementById('icon-container').style.backgroundColor = `${color}`;
 }
