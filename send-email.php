c2b57cc62248b571a2aebfd9eb201ec64f1a0bc4
Matthias Schmitz <m6schmitz123@gmail.com>
2023-05-08 02:59:47 +0200

    changes

--- send-email.php
+++ send-email.php
@@ -2,12 +2,12 @@
 
 if (isset($_POST['email'])) {
   $to = $_POST['email'];
-  $subject = 'Test email';
+  $subject = 'Invite to Join';
   $message = 'Are you tired of struggling to manage your projects?
               Do you want a reliable and efficient tool to help you plan, track, and complete your projects with ease?
               If so, we have the perfect solution for you: Join!';
-  $headers = 'From: blizz2mail@web.de' . "\r\n" .
-             'Reply-To: your-email@example.com' . "\r\n" .
+  $headers = 'From: Join-mail@web.de' . "\r\n" .
+             'Reply-To: Join-mail@web.de' . "\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
   if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
